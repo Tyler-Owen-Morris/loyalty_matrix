614042f5cccf802bd0d5364536c59a5bf1a12dc4
flicknewb <theowenmorris@gmail.com>
2023-04-18 17:50:36 -0700

    initial commit

---
+++ plugins/loyalty_matrix/src/data-configuration-panel.jsx
@@ -0,0 +1,541 @@
+import "./styles.css";
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import WithClient from "./with-client";
+import {
+  Button,
+  Label,
+  LoadingSpinner,
+  SelectMenu,
+  MenuItem,
+} from "@qualtrics/ui-react";
+
+export default function DataConfigurationPanel({ client, configuration }) {
+  const [definition, setDefinition] = useState();
+
+  useEffect(() => {
+    console.log("full config:", configuration);
+    let canceled = false;
+    if (!definition) {
+      const fetchDefinition = async () => {
+        const { fieldsetDefinition } = await client.postMessage(
+          "getDataSourceDefinition"
+        );
+        if (canceled) {
+          return;
+        }
+        setDefinition(fieldsetDefinition);
+      };
+      fetchDefinition();
+    }
+    return () => {
+      canceled = true;
+    };
+  }, []);
+
+  if (!definition) {
+    return (
+      <div className="spinner">
+        <LoadingSpinner show size="medium" />
+      </div>
+    );
+  }
+
+  let metric;
+  let metric2;
+  let dimension;
+  if (configuration) {
+    const { metrics, axes } = configuration;
+    console.log("configuration:", configuration);
+    if (metrics) {
+      metric = configuration.metrics[0];
+      metric2 = configuration.metrics[1];
+      console.log("mymetrics", metrics);
+    }
+    if (axes) {
+      dimension = configuration.axes[0].dimensions[0];
+    }
+  }
+
+  return (
+    <>
+      <FieldSelectMenu
+        client={client}
+        label={client.getText("configurationPanel.metric")}
+        defaultValue={metric && metric.field}
+        fields={getFieldsOfType("ScalarValue", "EnumerableScalarValue")}
+        placement="top-start"
+        onChange={onMetricChange}
+      />
+      <FieldSelectMenu
+        client={client}
+        label={client.getText("configurationPanel.metric2")}
+        defaultValue={metric2 && metric2.field}
+        fields={getFieldsOfType("ScalarValue", "EnumerableScalarValue")}
+        placement="top-start"
+        onChange={onMetricChange}
+      />
+      {/* <FieldSelectMenu
+        client={client}
+        label={client.getText("configurationPanel.dimension")}
+        defaultValue={dimension && dimension.fieldId}
+        fields={getFieldsOfType("ScalarValue", "EnumerableScalarValue")}
+        placement="bottom-start"
+        onChange={onDimensionChange}
+      /> */}
+    </>
+  );
+
+  function onMetricChange(field) {
+    change((configuration) => ({
+      ...configuration,
+      metrics: [
+        {
+          id: "metric",
+          label: "metric1",
+          field: "DM_1s583fa",
+          function: "avg",
+        },
+        {
+          id: "metric2",
+          label: "metric2",
+          field: "DM_6qorhx4",
+          function: "avg",
+        },
+        {
+          type: "custom",
+          id: "truly-loyal",
+          label: "Truly Loyal",
+          equation: "Loyal / Total",
+          function: "count",
+          metrics: [
+            {
+              id: "Loyal",
+              field: "_recordId",
+              function: "count",
+              label: "Loyal Count",
+              filters: [
+                {
+                  type: "filter",
+                  filter: {
+                    field: "DM_6qorhx4",
+                    operator: "greaterThanOrEqualTo",
+                    operand: 4,
+                  },
+                },
+                {
+                  type: "filter",
+                  filter: {
+                    field: "DM_1s583fa",
+                    operator: "greaterThanOrEqualTo",
+                    operand: 4,
+                  },
+                },
+              ],
+            },
+            {
+              id: "Total",
+              field: "_recordId",
+              label: "Total",
+              function: "count",
+            },
+          ],
+        },
+        {
+          type: "custom",
+          id: "trapped",
+          label: "Trapped",
+          equation: "Trapped / Total",
+          function: "count",
+          metrics: [
+            {
+              id: "Trapped",
+              field: "_recordId",
+              function: "count",
+              label: "Trapped Count",
+              filters: [
+                {
+                  type: "filter",
+                  filter: {
+                    field: "DM_6qorhx4",
+                    operator: "greaterThanOrEqualTo",
+                    operand: 4,
+                  },
+                },
+                {
+                  type: "filter",
+                  filter: {
+                    field: "DM_1s583fa",
+                    operator: "lessThan",
+                    operand: 4,
+                  },
+                },
+              ],
+            },
+            {
+              id: "Total",
+              field: "_recordId",
+              label: "Total",
+              function: "count",
+            },
+          ],
+        },
+        {
+          type: "custom",
+          id: "accessible",
+          label: "Accessible",
+          equation: "Accessible / Total",
+          function: "count",
+          metrics: [
+            {
+              id: "Accessible",
+              field: "_recordId",
+              function: "count",
+              label: "Accessible Count",
+              filters: [
+                {
+                  type: "filter",
+                  filter: {
+                    field: "DM_6qorhx4",
+                    operator: "lessThan",
+                    operand: 4,
+                  },
+                },
+                {
+                  type: "filter",
+                  filter: {
+                    field: "DM_1s583fa",
+                    operator: "greaterThanOrEqualTo",
+                    operand: 4,
+                  },
+                },
+              ],
+            },
+            {
+              id: "Total",
+              field: "_recordId",
+              label: "Total",
+              function: "count",
+            },
+          ],
+        },
+        {
+          type: "custom",
+          id: "high-risk",
+          label: "High Risk",
+          equation: "HighRisk / Total",
+          function: "count",
+          metrics: [
+            {
+              id: "HighRisk",
+              field: "_recordId",
+              function: "count",
+              label: "High Risk Count",
+              filters: [
+                {
+                  type: "filter",
+                  filter: {
+                    field: "DM_6qorhx4",
+                    operator: "lessThan",
+                    operand: 4,
+                  },
+                },
+                {
+                  type: "filter",
+                  filter: {
+                    field: "DM_1s583fa",
+                    operator: "lessThan",
+                    operand: 4,
+                  },
+                },
+              ],
+            },
+            {
+              id: "Total",
+              field: "_recordId",
+              label: "Total",
+              function: "count",
+            },
+          ],
+        },
+      ],
+      axes: [
+        {
+          id: "z-axis",
+          label: "Z-Axis",
+          dimensions: [
+            {
+              id: "metrics",
+              label: "filler dimension",
+              fieldId: "progress",
+            },
+          ],
+        },
+      ],
+    }));
+    console.log("after change config:", configuration);
+  }
+
+  // function onMetric2Change(field) {
+  //   change((configuration) => ({
+  //     ...configuration,
+  //     metrics: [
+  //       {
+  //         id: "metric",
+  //         label: "metric1",
+  //         field: "DM_1s583fa",
+  //         function: "avg",
+  //       },
+  //       {
+  //         id: "metric2",
+  //         label: "metric2",
+  //         field: "DM_6qorhx4",
+  //         function: "avg",
+  //       },
+  //       {
+  //         type: "custom",
+  //         id: "truly-loyal",
+  //         label: "Truly Loyal",
+  //         equation: "Loyal / Total",
+  //         function: "count",
+  //         metrics: [
+  //           {
+  //             id: "Loyal",
+  //             field: "_recordId",
+  //             function: "count",
+  //             label: "Loyal Count",
+  //             filters: [
+  //               {
+  //                 type: "filter",
+  //                 filter: {
+  //                   field: "DM_6qorhx4",
+  //                   operator: "greaterThanOrEqualTo",
+  //                   operand: 4,
+  //                 },
+  //               },
+  //               {
+  //                 type: "filter",
+  //                 filter: {
+  //                   field: "DM_1s583fa",
+  //                   operator: "greaterThanOrEqualTo",
+  //                   operand: 4,
+  //                 },
+  //               },
+  //             ],
+  //           },
+  //           {
+  //             id: "Total",
+  //             field: "_recordId",
+  //             label: "Total",
+  //             function: "count",
+  //           },
+  //         ],
+  //       },
+  //       {
+  //         type: "custom",
+  //         id: "trapped",
+  //         label: "Trapped",
+  //         equation: "Trapped / Total",
+  //         function: "count",
+  //         metrics: [
+  //           {
+  //             id: "Trapped",
+  //             field: "_recordId",
+  //             function: "count",
+  //             label: "Trapped Count",
+  //             filters: [
+  //               {
+  //                 type: "filter",
+  //                 filter: {
+  //                   field: "DM_6qorhx4",
+  //                   operator: "greaterThanOrEqualTo",
+  //                   operand: 4,
+  //                 },
+  //               },
+  //               {
+  //                 type: "filter",
+  //                 filter: {
+  //                   field: "DM_1s583fa",
+  //                   operator: "lessThan",
+  //                   operand: 4,
+  //                 },
+  //               },
+  //             ],
+  //           },
+  //           {
+  //             id: "Total",
+  //             field: "_recordId",
+  //             label: "Total",
+  //             function: "count",
+  //           },
+  //         ],
+  //       },
+  //       {
+  //         type: "custom",
+  //         id: "accessible",
+  //         label: "Accessible",
+  //         equation: "Accessible / Total",
+  //         function: "count",
+  //         metrics: [
+  //           {
+  //             id: "Accessible",
+  //             field: "_recordId",
+  //             function: "count",
+  //             label: "Accessible Count",
+  //             filters: [
+  //               {
+  //                 type: "filter",
+  //                 filter: {
+  //                   field: "DM_6qorhx4",
+  //                   operator: "lessThan",
+  //                   operand: 4,
+  //                 },
+  //               },
+  //               {
+  //                 type: "filter",
+  //                 filter: {
+  //                   field: "DM_1s583fa",
+  //                   operator: "greaterThanOrEqualTo",
+  //                   operand: 4,
+  //                 },
+  //               },
+  //             ],
+  //           },
+  //           {
+  //             id: "Total",
+  //             field: "_recordId",
+  //             label: "Total",
+  //             function: "count",
+  //           },
+  //         ],
+  //       },
+  //       {
+  //         type: "custom",
+  //         id: "high-risk",
+  //         label: "High Risk",
+  //         equation: "high-risk / Total",
+  //         function: "count",
+  //         metrics: [
+  //           {
+  //             id: "high-risk",
+  //             field: "_recordId",
+  //             function: "count",
+  //             label: "High Risk Count",
+  //             filters: [
+  //               {
+  //                 type: "filter",
+  //                 filter: {
+  //                   field: "DM_6qorhx4",
+  //                   operator: "lessThan",
+  //                   operand: 4,
+  //                 },
+  //               },
+  //               {
+  //                 type: "filter",
+  //                 filter: {
+  //                   field: "DM_1s583fa",
+  //                   operator: "lessThan",
+  //                   operand: 4,
+  //                 },
+  //               },
+  //             ],
+  //           },
+  //           {
+  //             id: "Total",
+  //             field: "_recordId",
+  //             label: "Total",
+  //             function: "count",
+  //           },
+  //         ],
+  //       },
+  //     ],
+  //   }));
+  //   console.log("after change config:", configuration);
+  // }
+
+  function onDimensionChange(field) {
+    change((configuration) => ({
+      ...configuration,
+      axes: [
+        {
+          id: "x-axis",
+          label: "X-Axis",
+          dimensions: [
+            {
+              id: "x-axis-dimension",
+              label: field.name,
+              fieldId: field.fieldId,
+            },
+          ],
+        },
+        {
+          id: "z-axis",
+          label: "Z-Axis",
+          dimensions: [
+            {
+              id: "metrics",
+              label: "filler dimension",
+              fieldId: "progress",
+            },
+          ],
+        },
+      ],
+    }));
+  }
+
+  function change(map) {
+    console.log("definition", definition);
+    const newConfiguration = {
+      ...map(configuration),
+      component: "fieldsets-aggregate",
+      fieldsetId: definition.fieldSetId,
+    };
+
+    newConfiguration.isComplete =
+      newConfiguration.metrics && newConfiguration.axes;
+
+    console.log("newConfiguration:", newConfiguration);
+    client.postMessage("onDataConfigurationChange", newConfiguration);
+  }
+
+  function getFieldsOfType(...types) {
+    return definition.fieldSetView.filter((field) =>
+      types.includes(field.type)
+    );
+  }
+}
+
+function FieldSelectMenu({
+  client,
+  defaultValue,
+  fields,
+  label,
+  onChange,
+  placement,
+}) {
+  console.log("Label ->", label);
+  if (label == "Chart Style") {
+    return <></>;
+  }
+  return (
+    <div className="form-group">
+      <Label className="label">{label}</Label>
+      <SelectMenu
+        defaultValue={defaultValue}
+        defaultLabel={client.getText("configurationPanel.selectAField")}
+        placement={placement}
+        maxHeight="100px"
+        disabled={fields.length === 0}
+        onChange={(fieldId) => {
+          onChange(fields.find((field) => field.fieldId === fieldId));
+        }}
+      >
+        {fields.map(({ fieldId, name }) => (
+          <MenuItem key={fieldId} className="menu-item" value={fieldId}>
+            {name}
+          </MenuItem>
+        ))}
+      </SelectMenu>
+    </div>
+  );
+}
---
+++ plugins/loyalty_matrix/src/visualization.jsx
@@ -0,0 +1,171 @@
+import { Label } from "@qualtrics/ui-react";
+import React, { useState, useEffect, Text } from "react";
+import ReactDOM from "react-dom";
+import WithClient from "./with-client";
+import render from "./render";
+import Chart from "chart.js";
+import isEqual from "lodash/isEqual";
+
+function Visualization() {
+  return (
+    <WithClient>
+      {(props) => (
+        // <Content
+        //   // Force re-mount when chart type has changed
+        //   key={props.viewConfiguration.chartType}
+        //   {...props}
+        // />
+        <TextContent key={props.viewconfiguration} {...props}></TextContent>
+      )}
+    </WithClient>
+  );
+}
+
+function TextContent({ data, viewConfiguration }) {
+  const [ref, setRef] = useState();
+  console.log("data:", data);
+  // let mets = [];
+  // for (let i = 0; i < 4; i++) {
+  //   console.log(data.data[i]);
+  //   mets.push(data.data[0].children[i].label);
+  // }
+  let loyal = "0",
+    acces = "0",
+    trap = "0",
+    risk = "0";
+  let mydata = data.data[0].children;
+  for (let i = 0; i < mydata.length; i++) {
+    let myId = mydata[i].id;
+    let myVal = (mydata[i].value * 100).toFixed(1);
+    if (myId == "truly-loyal") {
+      loyal = myVal.toString();
+    }
+    if (myId == "trapped") {
+      trap = myVal.toString();
+    }
+    if (myId == "accessible") {
+      acces = myVal.toString();
+    }
+    if (myId == "high-risk") {
+      risk = myVal.toString();
+    }
+  }
+  return (
+    <>
+      <table className="matrix-table">
+        <tbody>
+          <tr>
+            <td className="square-cell">
+              <p className="square-header">Truly Loyal</p>
+              <p className="square-text">{loyal}%</p>
+            </td>
+            <td className="square-cell">
+              <p className="square-header">Accessible</p>
+              <p className="square-text">{acces}%</p>
+            </td>
+          </tr>
+          <tr>
+            <td className="square-cell">
+              <p className="square-header">Trapped</p>
+              <p className="square-text">{trap}%</p>
+            </td>
+            <td className="square-cell">
+              <p className="square-header">High Risk</p>
+              <p className="square-text">{risk}%</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      {/* <canvas
+        ref={(ref) => {
+          if (ref) {
+            setRef(ref);
+          }
+        }}
+      ></canvas> */}
+    </>
+  );
+}
+
+function Content({ data, viewConfiguration }) {
+  console.log("data:", data);
+  const [ref, setRef] = useState();
+  const [chart, setChart] = useState();
+
+  useEffect(() => {
+    if (ref) {
+      const config = getConfig();
+      if (!chart) {
+        setChart(new Chart(ref, config));
+      } else {
+        chart.data = config.data;
+        chart.update();
+      }
+    }
+  }, [ref, data, viewConfiguration]);
+
+  return (
+    <canvas
+      ref={(ref) => {
+        if (ref) {
+          setRef(ref);
+        }
+      }}
+    ></canvas>
+  );
+
+  function getConfig() {
+    return {
+      data: transformData(data),
+      type: viewConfiguration.chartType || "bar",
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    };
+  }
+}
+
+function transformData(cube) {
+  const { members, label } = cube.axes[0].dimensions[0];
+  const labels = [];
+  const dataset = {
+    data: [],
+    label,
+    backgroundColor: [],
+    borderColor: [],
+    hoverBackgroundColor: [],
+    hoverBorderColor: [],
+    borderWidth: 1,
+  };
+
+  cube.data.forEach((datum, index) => {
+    labels.push(members[datum.id].label);
+    dataset.data.push(datum.value);
+    const color = [0, 0, 0].map((_, i) => rainbow(i, index));
+    dataset.backgroundColor.push(`rgba(${color}, 0.2)`);
+    dataset.borderColor.push(`rgba(${color}, 1)`);
+    dataset.hoverBackgroundColor.push(`rgba(${color}, .8)`);
+  });
+
+  return {
+    labels,
+    datasets: [dataset],
+  };
+}
+
+function rainbow(color, index) {
+  const frequency = 0.9;
+  return Math.round(Math.sin(frequency * index + color * 2) * 127 + 128);
+}
+
+render(Visualization);
